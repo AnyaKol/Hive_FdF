a521d5c719dd78d49b2863e4fac0bc37e5e17ef7
Anna Kolupaeva <akolupae@c2r3p1.hive.fi>
2025-08-08 19:12:01 +0300

    Linked MLX42 in Makefile Made first check for number of args and map

--- fdf.c
+++ fdf.c
@@ -6,21 +6,83 @@
 /*   By: akolupae <akolupae@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 19:24:30 by akolupae          #+#    #+#             */
-/*   Updated: 2025/08/07 21:46:49 by akolupae         ###   ########.fr       */
+/*   Updated: 2025/08/08 19:09:21 by akolupae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(void)
+void	check_args(int argc, char **argv);
+int		count_numbers(char *line);
+
+int	main(int argc, char **argv)
 {
 	static mlx_image_t	*image;
 	mlx_t	*mlx;
 
+	check_args(argc, argv);
+	ft_printf("Good map!\n");
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	image = mlx_new_image(mlx, 128, 128);
-	mlx_mage_to_window(mlx, image, 0, 0);
+	mlx_image_to_window(mlx, image, 0, 0);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (0);
+}
+
+void	check_args(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+	int		num;
+
+	if (argc != 2)
+	{
+		ft_printf("Invalid number of arguments\n");
+		exit(1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	num = 0;
+	while (true)
+	{
+		line = get_next_line(fd);
+		if (!ft_strncmp(line, "(null)", 6))
+			break ;
+		if (num == 0)
+			num = count_numbers(line);
+		if (num <= 0 || num != count_numbers(line))
+		{
+			ft_printf("Invalid map formatting\n");
+			ft_free(line);
+			close(fd);
+			exit (1);
+		}
+		ft_free(line);
+	}
+	ft_free(line);
+	close(fd);
+}
+
+int	count_numbers(char *line)
+{
+	int	num;
+	int	i;
+
+	i = 0;
+	num = 0;
+	while (line[i] == ' ')
+		i++;
+	while (line[i] != '\n' && line[i] != '\0')
+	{
+		if (line[i] == '-')
+			i++;
+		if (!ft_isdigit(line[i]))
+			return (-1);
+		num++;
+		while (ft_isdigit(line[i]))
+			i++;
+		while (line[i] == ' ')
+			i++;
+	}
+	return (num);
 }
